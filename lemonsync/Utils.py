a4359e1cdae9e9b2ea6ce3106229c5080dd22885
Michael Breedlove <mbreedlove@users.noreply.github.com>
2016-06-07 13:22:06 +0700

    List each deleted file during remove_remote_files

--- lemonsync/Utils.py
+++ lemonsync/Utils.py
@@ -244,6 +244,7 @@ class Utils ():
 		# rest the remote theme
 		for key in keys:
 			key.delete()
+			print(Fore.RED + '[' + time.strftime("%c") + '] Successfully deleted ' + key.name.replace(path, '') + Style.RESET_ALL)
 
 		return
 
